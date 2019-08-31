5c2bace7fa5eabea2e353af856a4527fdd8332a4
depinv <guido.grogger@googlemail.com>
2019-08-31 09:36:38 +0200

    fixed error in serving index.html

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.sendFile("index.html");
+  res.sendFile("index.html", { root: __dirname });
 });
 
 module.exports = {
