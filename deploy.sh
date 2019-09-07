adcb4f2874aa49e1ed044b9aecc6bfc914a7f312
depinv <guido.grogger@googlemail.com>
2019-09-07 05:33:09 +0000

    fixed typo

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 # deploy as function in google cloud
-PROJECT="meeting_panda"
+PROJECT="meeting-panda"
 
 gcloud config set project $PROJECT
 gcloud functions deploy app --runtime nodejs8 --trigger-http
